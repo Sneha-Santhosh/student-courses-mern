aee2161bd7532732a699fe24ddd811b4e2e1202a
ss <45783979+Sneha-Santhosh@users.noreply.github.com>
2021-07-31 23:39:20 -0400

    initial commit

---
+++ app/models/course.server.model.js
@@ -0,0 +1,26 @@
+﻿const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const CourseSchema = new Schema({
+    courseCode: {
+        type: String,
+        default: ''
+    },
+    courseName: {
+        type: String,
+        default: '',
+        trim: true,
+        required: 'Course Name cannot be blank'
+    },
+    section: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    semester: {
+        type: String,
+        default: '',
+        trim: true
+    },
+    students: [{ type: Schema.Types.ObjectId, ref: 'Student' }]
+});
+mongoose.model('Course', CourseSchema);
